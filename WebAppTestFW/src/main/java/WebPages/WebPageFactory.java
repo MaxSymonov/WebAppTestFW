2a72f271885751daa2b5d10900d308afdd0f6856
MaxSymonov <56004791+MaxSymonov@users.noreply.github.com>
2024-06-25 17:46:25 +0300

    Delete WebAppTestFW directory

--- WebAppTestFW/src/main/java/WebPages/WebPageFactory.java
+++
@@ -1,9 +0,0 @@
-package WebPages;
-
-import WebAppBase.WebApplicationManager;
-import WebAppBase.WebAppBase;
-
-public class WebPageFactory extends WebAppBase {
-    public LoginPage loginPage() {return new LoginPage(WebApplicationManager.getWebDriver());}
-    public MainPage mainPage() {return new MainPage(WebApplicationManager.getWebDriver());}
-}
