00e6403106af7a0df7add6d642a539043c259b31
MaxSymonov <56004791+MaxSymonov@users.noreply.github.com>
2024-06-26 15:50:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("io.github.bonigarcia:webdrivermanager:5.8.0")
     implementation("org.yaml:snakeyaml:2.2")
     implementation("org.slf4j:slf4j-api:2.0.13")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 tasks.withType<Test> {
